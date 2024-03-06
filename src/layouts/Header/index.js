dd7c09838e6dfe11cc6df8d74f5577a78932ed3b
larahope <138026315+javierstere@users.noreply.github.com>
2024-03-05 18:46:02 -0600

    completed navbar

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,91 +1,95 @@
 export default function Header() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-info p-3">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          DBook Inc
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <div className="container">
+      <div className="col-10 offset-1">
+        <nav className="navbar navbar-expand-lg navbar-light bg-info p-3">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              DBook Inc
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav ms-auto ">
-            <li class="nav-item">
-              <a class="nav-link mx-2 active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2" href="#">
-                Products
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2" href="#">
-                Pricing
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link mx-2 dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Company
-              </a>
-              <ul
-                class="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Blog
+            <div className=" collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav ms-auto ">
+                <li className="nav-item">
+                  <a className="nav-link mx-2 active" aria-current="page" href="#">
+                    Home
                   </a>
                 </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    About Us
+                <li className="nav-item">
+                  <a className="nav-link mx-2" href="#">
+                    Products
                   </a>
                 </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Contact us
+                <li className="nav-item">
+                  <a className="nav-link mx-2" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link mx-2 dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Company
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Contact us
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <ul className="navbar-nav ms-auto d-none d-lg-inline-flex">
+                <li className="nav-item mx-2">
+                  <a className="nav-link text-dark h5" href="" target="blank">
+                    <i className="fab fa-google-plus-square"></i>
+                  </a>
+                </li>
+                <li className="nav-item mx-2">
+                  <a className="nav-link text-dark h5" href="" target="blank">
+                    <i className="fab fa-twitter"></i>
+                  </a>
+                </li>
+                <li className="nav-item mx-2">
+                  <a className="nav-link text-dark h5" href="" target="blank">
+                    <i className="fab fa-facebook-square"></i>
                   </a>
                 </li>
               </ul>
-            </li>
-          </ul>
-          <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
-            <li class="nav-item mx-2">
-              <a class="nav-link text-dark h5" href="" target="blank">
-                <i class="fab fa-google-plus-square"></i>
-              </a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link text-dark h5" href="" target="blank">
-                <i class="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link text-dark h5" href="" target="blank">
-                <i class="fab fa-facebook-square"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
+            </div>
+          </div>
+        </nav>
       </div>
-    </nav>
+    </div>
   );
 }
--- src/layouts/Navbar/index.js
+++ src/layouts/Navbar/index.js
@@ -1,91 +1,28 @@
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-info p-3">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          DBook Inc
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav ms-auto ">
-            <li class="nav-item">
-              <a class="nav-link mx-2 active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2" href="#">
-                Products
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2" href="#">
-                Pricing
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link mx-2 dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Company
-              </a>
-              <ul
-                class="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Blog
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    About Us
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Contact us
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
-            <li class="nav-item mx-2">
-              <a class="nav-link text-dark h5" href="" target="blank">
-                <i class="fab fa-google-plus-square"></i>
-              </a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link text-dark h5" href="" target="blank">
-                <i class="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li class="nav-item mx-2">
-              <a class="nav-link text-dark h5" href="" target="blank">
-                <i class="fab fa-facebook-square"></i>
-              </a>
-            </li>
-          </ul>
+    <div className="bg-primary">
+      <div className="container">
+        <div className="col-10 offset-1 d-flex justify-content-between align-items-center">
+          <div>
+            <span className="text-white">
+              <u>Application in Progress</u>
+            </span>
+            <span className="text-white ms-3">Returned Applications</span>
+          </div>
+          <div>
+            <div className="d-flex align-items-center gap-3">
+              <div className="text-white text-end">
+                <div className="fs-7">User Name</div>
+                <div className="fs-7">User Company Name</div>
+              </div>
+              <i className="bi bi-person text-white fs-3"></i>
+              <i className="bi bi-gear text-white fs-3"></i>
+              <i className="bi bi-question-circle text-white fs-3"></i>
+              <i className="bi bi-box-arrow-right text-white fs-3"></i>
+            </div>
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
