ab136e98f411143af300a5258f060d03a7dd50e5
larahope <138026315+javierstere@users.noreply.github.com>
2024-03-05 18:05:47 -0600

    added navbar

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Navbar from "./Navbar";
 export default function Layout() {
   return (
     <>
       <Header />
+      <Navbar />
       <Outlet />
     </>
   );
